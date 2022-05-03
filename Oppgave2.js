821279602a3595ec5309a68bcbe529f282a5a6a7
Midori04 <machineko303@gmail.com>
2022-05-03 11:45:08 +0200

    first commit

---
+++ Oppgave2.js
@@ -0,0 +1,28 @@
+// 20
+
+// if (a >10 )
+// ((a-10)*2)
+
+// else {
+//     console.log(10-a)
+//     console.log('10 -',a,' = ', (10-a))
+// }
+
+var Ettall= window.prompt("Skriv et tall");
+
+
+
+if (Ettall > 10) {
+    console.log((Ettall-10)*2);
+} else if (Ettall == 10) {
+    console.log(Ettall-10);
+} else if (Ettall < 10) {
+    console.log(10 - Ettall);
+}
+
+a = 7;
+console.log('10 -',a,' = ', (10-a))
+
+a = 10;
+console.log('10 -',a,' = ', (10-a))
+
